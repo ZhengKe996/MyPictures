79572847ed1bd0bc622a037d08cc27bcbddeddcf
zhengke <zhengke996@163.com>
2025-01-19 20:18:38 +0800

    feat(frontend): 添加 dayjs 依赖并配置国际化
    - 在 package.json 中添加 dayjs 依赖
    - 在 App.vue 中引入并配置 ant-design-vue 的国际化支持
    - 添加默认头像图片资源

--- snake-fronted/src/App.vue
+++ snake-fronted/src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+const locale = zhCN;
+
 import BasicLayout from "@/layouts/BasicLayout.vue";
 import { LayoutMenu } from "@/config";
 import { useRoute } from "vue-router";
@@ -6,11 +9,13 @@ const route = useRoute();
 </script>
 
 <template>
-  <template v-if="route.meta?.layout === LayoutMenu.BasicLayout">
-    <BasicLayout />
-  </template>
-  <template v-else>
-    <RouterView />
-  </template>
+  <a-config-provider :locale="locale">
+    <template v-if="route.meta?.layout === LayoutMenu.BasicLayout">
+      <BasicLayout />
+    </template>
+    <template v-else>
+      <RouterView />
+    </template>
+  </a-config-provider>
 </template>
 <style scoped></style>
