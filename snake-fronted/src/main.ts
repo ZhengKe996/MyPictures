c2973deb791bbb0adb3a2978962789c49ac89749
zhengke <zhengke996@163.com>
2025-01-19 19:48:34 +0800

    build(snake-fronted): 添加 pinia-plugin-persistedstate 依赖
    - 在 package.json 中新增 pinia-plugin-persistedstate 依赖，版本为 ^4.2.0
    - 此依赖用于实现 Pinia 的持久化状态管理功能

--- snake-fronted/src/main.ts
+++ snake-fronted/src/main.ts
@@ -5,9 +5,11 @@ import router from "./router";
 
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import "virtual:uno.css";
 import "normalize.css";
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 createApp(App).use(Antd).use(pinia).use(router).mount("#app");
