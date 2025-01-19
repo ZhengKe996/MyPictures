21f78c621237353a861befc945b48693f1f8f7a9
zhengke <zhengke996@163.com>
2025-01-19 19:29:57 +0800

    build(snake-fronted): 集成 Ant Design 组件库
    - 在 main.ts 中引入 Antd 和其样式文件
    - 在 createApp 中使用 Antd

--- snake-fronted/src/main.ts
+++ snake-fronted/src/main.ts
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+
 import "virtual:uno.css";
 import "normalize.css";
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount("#app");
+createApp(App).use(Antd).use(pinia).use(router).mount("#app");
