0a8b62b0a4799f8ea648c29825ac314f9a2f632d
zhengke <zhengke996@163.com>
2025-01-21 18:57:53 +0800

    feat(layout): 添加返回登录/注册功能
    - 新增 AppBackToLogin 组件，用于在用户布局中添加返回登录或注册页面的按钮
    - 在 UserLayout 中集成 AppBackToLogin 组件
    - 实现了根据当前路由动态显示"登录"或"注册"按钮的功能

--- snake-fronted/src/layouts/UserLayout.vue
+++ snake-fronted/src/layouts/UserLayout.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>
+  <!-- <div>
     <router-view></router-view>
+  </div> -->
+
+  <div class="bg-fcfcfc text-12161e h-full">
+    <div class="bg-fcfcfc text-12161e h-screen overflow-y-auto relative">
+      <div
+        class="relative min-h-screen flex justify-center items-center box-border"
+      >
+        <AppBackToLogin />
+        <div class="relative flex-grow">
+          <div class="max-w-100 w-full my-0 mx-auto py-0 px-5 box-border">
+            <RouterView />
+          </div>
+        </div>
+        <p
+          class="transition duration-200 m-0 absolute z-10 left-2/4 text-center translate-x-[-50%] bottom-6 text-[10px] max-w-[260px] text-[#c2c3c7]"
+        >
+          This site is authenticate by supabase.com and the Supabase
+
+          <a
+            class="no-underline transition duration-200 text-[#a3a5ab] hover:text-[#656976]"
+            target="_blank"
+            href="#"
+            rel="noreferrer"
+          >
+            Privacy Policy
+          </a>
+          and
+          <a
+            class="no-underline transition duration-200 text-[#a3a5ab] hover:text-[#656976]"
+            target="_blank"
+            href="#"
+            rel="noreferrer"
+          >
+            Terms of Service
+          </a>
+          apply.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import AppBackToLogin from "@/components/AppBackToLogin";
+</script>
