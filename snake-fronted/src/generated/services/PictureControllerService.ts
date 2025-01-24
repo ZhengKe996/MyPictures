bece0d2a7315245e27cd03d72a1db2cd5194a069
zhengke <zhengke996@163.com>
2025-01-24 21:13:16 +0800

    refactor(snake-fronted): 调整代码格式和参数类型
    - 重新格式化了 PictureControllerService 类中的代码，使代码风格更加一致
    - 将部分参数类型从 number 改为 string，以适应可能的类型变化

--- snake-fronted/src/generated/services/PictureControllerService.ts
+++ snake-fronted/src/generated/services/PictureControllerService.ts
@@ -2,211 +2,211 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { BaseResponse_boolean_ } from '../models/BaseResponse_boolean_';
-import type { BaseResponse_Page_Picture_ } from '../models/BaseResponse_Page_Picture_';
-import type { BaseResponse_Page_PictureVO_ } from '../models/BaseResponse_Page_PictureVO_';
-import type { BaseResponse_Picture_ } from '../models/BaseResponse_Picture_';
-import type { BaseResponse_PictureTagCategory_ } from '../models/BaseResponse_PictureTagCategory_';
-import type { BaseResponse_PictureVO_ } from '../models/BaseResponse_PictureVO_';
-import type { DeleteRequest } from '../models/DeleteRequest';
-import type { PictureEditRequest } from '../models/PictureEditRequest';
-import type { PictureQueryRequest } from '../models/PictureQueryRequest';
-import type { PictureUpdateRequest } from '../models/PictureUpdateRequest';
-import type { CancelablePromise } from '../core/CancelablePromise';
-import { OpenAPI } from '../core/OpenAPI';
-import { request as __request } from '../core/request';
+import type { BaseResponse_boolean_ } from "../models/BaseResponse_boolean_";
+import type { BaseResponse_Page_Picture_ } from "../models/BaseResponse_Page_Picture_";
+import type { BaseResponse_Page_PictureVO_ } from "../models/BaseResponse_Page_PictureVO_";
+import type { BaseResponse_Picture_ } from "../models/BaseResponse_Picture_";
+import type { BaseResponse_PictureTagCategory_ } from "../models/BaseResponse_PictureTagCategory_";
+import type { BaseResponse_PictureVO_ } from "../models/BaseResponse_PictureVO_";
+import type { DeleteRequest } from "../models/DeleteRequest";
+import type { PictureEditRequest } from "../models/PictureEditRequest";
+import type { PictureQueryRequest } from "../models/PictureQueryRequest";
+import type { PictureUpdateRequest } from "../models/PictureUpdateRequest";
+import type { CancelablePromise } from "../core/CancelablePromise";
+import { OpenAPI } from "../core/OpenAPI";
+import { request as __request } from "../core/request";
 export class PictureControllerService {
-    /**
-     * deletePicture
-     * @param deleteRequest deleteRequest
-     * @returns BaseResponse_boolean_ OK
-     * @returns any Created
-     * @throws ApiError
-     */
-    public static deletePictureUsingPost(
-        deleteRequest: DeleteRequest,
-    ): CancelablePromise<BaseResponse_boolean_ | any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/picture/delete',
-            body: deleteRequest,
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * editPicture
-     * @param pictureEditRequest pictureEditRequest
-     * @returns BaseResponse_boolean_ OK
-     * @returns any Created
-     * @throws ApiError
-     */
-    public static editPictureUsingPost(
-        pictureEditRequest: PictureEditRequest,
-    ): CancelablePromise<BaseResponse_boolean_ | any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/picture/edit',
-            body: pictureEditRequest,
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * getPictureById
-     * @param id id
-     * @returns BaseResponse_Picture_ OK
-     * @throws ApiError
-     */
-    public static getPictureByIdUsingGet(
-        id?: number,
-    ): CancelablePromise<BaseResponse_Picture_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/picture/get',
-            query: {
-                'id': id,
-            },
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * getPictureVOById
-     * @param id id
-     * @returns BaseResponse_PictureVO_ OK
-     * @throws ApiError
-     */
-    public static getPictureVoByIdUsingGet(
-        id?: number,
-    ): CancelablePromise<BaseResponse_PictureVO_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/picture/get/vo',
-            query: {
-                'id': id,
-            },
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * listPictureByPage
-     * @param pictureQueryRequest pictureQueryRequest
-     * @returns BaseResponse_Page_Picture_ OK
-     * @returns any Created
-     * @throws ApiError
-     */
-    public static listPictureByPageUsingPost(
-        pictureQueryRequest: PictureQueryRequest,
-    ): CancelablePromise<BaseResponse_Page_Picture_ | any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/picture/list/page',
-            body: pictureQueryRequest,
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * listPictureVOByPage
-     * @param pictureQueryRequest pictureQueryRequest
-     * @returns BaseResponse_Page_PictureVO_ OK
-     * @returns any Created
-     * @throws ApiError
-     */
-    public static listPictureVoByPageUsingPost(
-        pictureQueryRequest: PictureQueryRequest,
-    ): CancelablePromise<BaseResponse_Page_PictureVO_ | any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/picture/list/page/vo',
-            body: pictureQueryRequest,
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * listPictureTagCategory
-     * @returns BaseResponse_PictureTagCategory_ OK
-     * @throws ApiError
-     */
-    public static listPictureTagCategoryUsingGet(): CancelablePromise<BaseResponse_PictureTagCategory_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/picture/tag_category',
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * updatePicture
-     * @param pictureUpdateRequest pictureUpdateRequest
-     * @returns BaseResponse_boolean_ OK
-     * @returns any Created
-     * @throws ApiError
-     */
-    public static updatePictureUsingPost(
-        pictureUpdateRequest: PictureUpdateRequest,
-    ): CancelablePromise<BaseResponse_boolean_ | any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/picture/update',
-            body: pictureUpdateRequest,
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-    /**
-     * uploadPicture
-     * @param file file
-     * @param id
-     * @returns BaseResponse_PictureVO_ OK
-     * @returns any Created
-     * @throws ApiError
-     */
-    public static uploadPictureUsingPost(
-        file: Blob,
-        id?: number,
-    ): CancelablePromise<BaseResponse_PictureVO_ | any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/api/picture/upload',
-            query: {
-                'id': id,
-            },
-            formData: {
-                'file': file,
-            },
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
+  /**
+   * deletePicture
+   * @param deleteRequest deleteRequest
+   * @returns BaseResponse_boolean_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static deletePictureUsingPost(
+    deleteRequest: DeleteRequest
+  ): CancelablePromise<BaseResponse_boolean_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/picture/delete",
+      body: deleteRequest,
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * editPicture
+   * @param pictureEditRequest pictureEditRequest
+   * @returns BaseResponse_boolean_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static editPictureUsingPost(
+    pictureEditRequest: PictureEditRequest
+  ): CancelablePromise<BaseResponse_boolean_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/picture/edit",
+      body: pictureEditRequest,
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * getPictureById
+   * @param id id
+   * @returns BaseResponse_Picture_ OK
+   * @throws ApiError
+   */
+  public static getPictureByIdUsingGet(
+    id?: string
+  ): CancelablePromise<BaseResponse_Picture_> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/picture/get",
+      query: {
+        id: id,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * getPictureVOById
+   * @param id id
+   * @returns BaseResponse_PictureVO_ OK
+   * @throws ApiError
+   */
+  public static getPictureVoByIdUsingGet(
+    id?: string
+  ): CancelablePromise<BaseResponse_PictureVO_> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/picture/get/vo",
+      query: {
+        id: id,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * listPictureByPage
+   * @param pictureQueryRequest pictureQueryRequest
+   * @returns BaseResponse_Page_Picture_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static listPictureByPageUsingPost(
+    pictureQueryRequest: PictureQueryRequest
+  ): CancelablePromise<BaseResponse_Page_Picture_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/picture/list/page",
+      body: pictureQueryRequest,
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * listPictureVOByPage
+   * @param pictureQueryRequest pictureQueryRequest
+   * @returns BaseResponse_Page_PictureVO_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static listPictureVoByPageUsingPost(
+    pictureQueryRequest: PictureQueryRequest
+  ): CancelablePromise<BaseResponse_Page_PictureVO_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/picture/list/page/vo",
+      body: pictureQueryRequest,
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * listPictureTagCategory
+   * @returns BaseResponse_PictureTagCategory_ OK
+   * @throws ApiError
+   */
+  public static listPictureTagCategoryUsingGet(): CancelablePromise<BaseResponse_PictureTagCategory_> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/picture/tag_category",
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * updatePicture
+   * @param pictureUpdateRequest pictureUpdateRequest
+   * @returns BaseResponse_boolean_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static updatePictureUsingPost(
+    pictureUpdateRequest: PictureUpdateRequest
+  ): CancelablePromise<BaseResponse_boolean_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/picture/update",
+      body: pictureUpdateRequest,
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+  /**
+   * uploadPicture
+   * @param file file
+   * @param id
+   * @returns BaseResponse_PictureVO_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static uploadPictureUsingPost(
+    file: Blob,
+    id?: string
+  ): CancelablePromise<BaseResponse_PictureVO_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/picture/upload",
+      query: {
+        id: id,
+      },
+      formData: {
+        file: file,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
 }
