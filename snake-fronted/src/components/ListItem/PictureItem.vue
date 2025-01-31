ca94ef01c47af19f68331e6c42d66542b20e1574
zhengke <zhengke996@163.com>
2025-01-31 09:24:21 +0800

    style(snake-fronted): 调整分享按钮大小
    - 在 PictureItem 组件中将分享按钮的 size 属性从 'lg' 修改为 'md'

--- snake-fronted/src/components/ListItem/PictureItem.vue
+++ snake-fronted/src/components/ListItem/PictureItem.vue
@@ -21,7 +21,7 @@
         class="hidden opacity-0 w-full h-full bg-zinc-900/50 absolute top-0 left-0 rounded duration-300 group-hover:opacity-100 xl:block"
       >
         <!-- 分享 -->
-        <Button class="absolute top-1.5 left-1.5" size="lg">分享</Button>
+        <Button class="absolute top-1.5 left-1.5" size="md">分享</Button>
 
         <!-- 点赞 -->
         <Button
